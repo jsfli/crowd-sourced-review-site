cacde43abaec425267d10e638bbe1cb9fc36fd92
Jeffrey Li <jeffrey.li@cantab.net>
2017-03-10 16:31:33 +0800

    finished review section

--- app/routes/index.js
+++ app/routes/index.js
@@ -2,6 +2,9 @@ import Ember from 'ember';
 
 export default Ember.Route.extend({
   model(){
-    return this.store.findAll('restaurant');
+    return Ember.RSVP.hash({
+      restaurants: this.store.findAll('restaurant'),
+      reviews: this.store.findAll('review')
+    });
   },
 });
